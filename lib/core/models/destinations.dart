e6b88b207e4e0cd2ca9e04c8ac6a9a6ce2b086c3
mari <potasticart@gmail.com>
2025-11-01 20:31:05 +0200

    GNav

--- lib/core/models/destinations.dart
+++ lib/core/models/destinations.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:tourista/core/utils/app_strings.dart';
 
 enum Destinations {
-  home(AppStrings.home, Icon(Icons.home)),
-  profile(AppStrings.profile, Icon(Icons.person)),
-  map(AppStrings.map, Icon(Icons.map));
+  home(AppStrings.home, Icons.home),
+  profile(AppStrings.profile, Icons.person),
+  map(AppStrings.map, Icons.map);
 
   final String name;
-  final Icon icon;
+  final IconData icon;
   const Destinations(this.name, this.icon);
 }
--- lib/presentation/widgets/nav_bar.dart
+++ lib/presentation/widgets/nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:tourista/core/models/destinations.dart';
 import 'package:tourista/core/theme/app_colors.dart';
 
@@ -10,22 +11,33 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        indicatorColor: AppColors.darkBlue,
-        selectedIndex: navigationShell.currentIndex,
-        onDestinationSelected: navigationShell.goBranch,
-        indicatorShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
+      bottomNavigationBar: Container(
+        height: 90,
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+          child: GNav(
+            padding: EdgeInsetsGeometry.all(10),
+            color: AppColors.white,
+            selectedIndex: navigationShell.currentIndex,
+            onTabChange: navigationShell.goBranch,
+            gap: 8,
+            activeColor: Colors.lightBlue.shade200,
+            tabBackgroundColor: Colors.grey.shade800,
+            tabBorderRadius: 50,
 
-        destinations: Destinations.values
-            .map(
-              (distnation) => NavigationDestination(
-                icon: distnation.icon,
-                label: distnation.name,
-              ),
-            )
-            .toList(),
+            tabActiveBorder: Border.all(
+              color: Colors.black45,
+            ), // tab button border
+
+            tabs: Destinations.values
+                .map(
+                  (distnation) =>
+                      GButton(icon: distnation.icon, text: distnation.name),
+                )
+                .toList(),
+          ),
+        ),
       ),
       body: navigationShell,
     );
